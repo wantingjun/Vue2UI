9befe56b8b873694ac020be7650a4a0a100e31a6
TingjunWan <wtj9631@163.com>
2022-03-16 00:04:49 +0800

    创建cascader

--- src/app.js
+++ src/app.js
@@ -15,32 +15,68 @@ import plugin from './plugin'
 import popover from './popover'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
-
+import Cascader from './cascader'
 
 Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
-Vue.component('g-input', Input)
-Vue.component('g-row', Row)
+Vue.component('g-cascader', Cascader)
 Vue.component('g-col', Col)
-Vue.component('g-layout', Layout)
-Vue.component('g-footer', Footer)
-Vue.component('g-sider', Sider)
-Vue.component('g-header', Header)
-Vue.component('g-content', Content)
-Vue.component('g-toast', Toast)
-Vue.component('g-popover', popover)
 Vue.component('g-collapse', Collapse)
 Vue.component('g-collapse-item', CollapseItem)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-header', Header)
+Vue.component('g-icon', Icon)
+Vue.component('g-input', Input)
+Vue.component('g-layout', Layout)
+Vue.component('g-popover', popover)
+Vue.component('g-row', Row)
+Vue.component('g-sider', Sider)
+// Vue.component('g-tabs', Tabs)
+// Vue.component('g-tabs-body', TabsBody)
+// Vue.component('g-tabs-head', TabsHead)
+// Vue.component('g-tabs-item', TabsItem)
+// Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-toast', Toast)
 
 Vue.use(plugin)
 
 new Vue({
   el: '#app',
   data:{
-    loading1:false,
-    message: 'hi',
-    selectedTab: ['2','1']
+    source: [{
+      name: '浙江',
+      children: [
+        {
+          name: '杭州',
+          children: [
+            {name: '上城'},
+            {name: '下城'},
+            {name: '江干'},
+          ]
+        },
+        {
+          name: '嘉兴',
+          children: [
+            {name: '南湖'},
+            {name: '秀洲'},
+            {name: '嘉善'},
+          ]
+        },
+      ]
+    }, {
+      name: '福建',
+      children: [
+        {
+          name: '福州',
+          children: [
+            {name: '鼓楼'},
+            {name: '台江'},
+            {name: '仓山'},
+          ]
+        },
+      ]
+    }]
   },
   created(){
     // setTimeout(()=>{
--- src/cascader.vue
+++ src/cascader.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="cascader">
-    <slot></slot>
+    <div class="trigger" @click="popoverVisible = !popoverVisible">
+    </div>
+    <div class="popover" v-if="popoverVisible">
+      <cascader-items :items="source"></cascader-items>
+    </div>
   </div>
 </template>
 
 <script>
+import CascaderItems from './cascader-items'
 export default {
-  name: 'GuluCascader'
+  name: 'GuluCascader',
+  components: {CascaderItems},
+  props: {
+    source: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      popoverVisible: false,
+    }
+  },
 }
 </script>
 
 <style scoped lang="scss">
 @import "var";
 .cascader {
+  .trigger {
+    border: 1px solid red;
+    height: 32px;
+    width: 100px;
+  }
+  .popover {
+    border: 2px solid green;
+    height: 200px;
+    display: flex;
+    .label {
+      white-space: nowrap;
+    }
+  }
 }
 </style>
